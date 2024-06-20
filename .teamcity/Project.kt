6af086fb55d2e2751fcb9420bd66a3e31fb4a965
Václav Beneš <vaclav.benes@gendigital.com>
2024-06-20 13:16:42 +0200

    Add whole project

---
+++ .teamcity/Project.kt
@@ -0,0 +1,26 @@
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.buildSteps.kotlinScript
+import jetbrains.buildServer.configs.kotlin.toId
+import java.io.File
+
+object Project : Project({
+    buildType(EvaluateKotlinScript)
+
+})
+
+object EvaluateKotlinScript: BuildType({
+    id = DslContext.createId("Evaluate".toId())
+    name = "Evaluate"
+
+    val dataFile = File(DslContext.baseDir,"script/github-token.main.kts")
+
+    steps {
+        kotlinScript {
+            name = "Evaluate Kotlin Script"
+            content = dataFile.readText()
+        }
+    }
+
+})
---
+++ .teamcity/script/github-token.main.kts
@@ -0,0 +1,6 @@
+@file:Repository("https://repo1.maven.org/maven2/")
+@file:DependsOn("com.google.code.gson:gson:2.10.1")
+
+import com.google.gson.Gson
+
+val gson = Gson()
